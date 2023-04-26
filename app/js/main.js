f25b7039575ef7f892c50080ed4722e87d59d59a
anicxxl <kopotiyvlad@gmail.com>
2023-04-26 18:48:30 +0300

    added product section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
 $(function () {
+  $('.product-tabs__link').on('click',function(e){
+    e.preventDefault();
+    $('.product-tabs__link').removeClass('product-tabs__link--active');
+		$(this).addClass('product-tabs__link--active');
+
+		$('.product-tabs__content-box').removeClass('product-tabs__content-box--active');
+		$($(this).attr('href')).addClass('product-tabs__content-box--active');
+
+  });
+
 
 });
